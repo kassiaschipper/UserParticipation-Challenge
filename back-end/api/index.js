2a1e51602ba86c637adc7af5a1d739685eb034bc
Kássia <kassia_guterres@outlook.com>
2023-05-31 00:16:46 -0300

    Index refactored.

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -1,43 +1,47 @@
-const { ApolloServer } =  require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer } = require("apollo-server-express");
+const mongoose = require("mongoose");
 const userSchema = require("./user/schema/user.graphql");
 const userResolvers = require("./user/resolvers/userResolvers");
-const UserAPI = require('./user/datasource/user')
-const dotenv = require('dotenv');
+const UserAPI = require("./user/datasource/user");
+const dotenv = require("dotenv");
+const express = require("express");
+const http = require("http");
 dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI;
 const typeDefs = [userSchema];
 const resolvers = [userResolvers];
 
+const app = express();
+
+
 const server = new ApolloServer({
-    cors: {
-        origin: "https://user-participation-challenge.vercel.app/",
-        credentials: true 
-    },
-    typeDefs,
-    resolvers, 
-    dataSources: () => {
-        return {
-            usersAPI: new UserAPI()
-        }
-    }
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    usersAPI: new UserAPI(),
+  }),
+  introspection: true,
 });
 
-mongoose
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app, path: "/" });
+
+  mongoose
     .connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-     })
-    .then(()=> {
-        console.log(`MongoDB connected`);
-        return server.listen({ port: Number.parseInt(process.env.PORT) || 5001});
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-    .then((res) => {
-        console.log(`Server lintening at ${res.url}}`)
+    .then(() => {
+      console.log(`MongoDB connected`);
+      app.listen({ port: process.env.PORT || 5001 }, () => {
+        console.log(`Server listening at ${process.env.PORT}`);
+      });
     })
-    .catch(err => {
-        console.log(err.message);
+    .catch((err) => {
+      console.log(err.message);
     });
+}
 
-
+startServer();
